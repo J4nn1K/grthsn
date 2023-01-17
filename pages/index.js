96916d2d178f15f6a767afc58662d93088bfa843
J4nn1K <jannikjorge@grothusen.de>
2023-01-17 00:03:36 -0800

    Remove facivon

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
         <title>Jannik Grothusen</title>
         {/* <meta name="description" content=""/> */}
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
       <main>
         <div className='p-[33px] h-screen bg-white dark:bg-black'>
