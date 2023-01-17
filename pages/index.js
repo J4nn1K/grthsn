e4e73f601b382b6ba981eed888010050632580e7
J4nn1K <jannikjorge@grothusen.de>
2023-01-17 10:46:18 -0800

    Add columns

--- pages/index.js
+++ pages/index.js
@@ -11,10 +11,32 @@ export default function Home() {
       </Head>
       <main>
         <div className='p-[33px] h-screen bg-white dark:bg-black'>
-          <h1 className='text-base mb-[22px] text-[#262626] dark:text-[#e6e6e6]'>Jannik Grothusen</h1>
-          <p className='text-base mb-[22px] text-[#262626] dark:text-[#e6e6e6]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras augue sapien, rutrum vitae lectus ut, aliquet tincidunt sem. Curabitur rutrum mi viverra, pulvinar ipsum id, aliquam dui. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
-          <p className='text-base mb-[22px] text-[#262626] dark:text-[#e6e6e6]'>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed cursus id purus in pretium. Duis porttitor eros pharetra sagittis mollis. Vivamus ut aliquet eros, ultricies interdum lectus. In mi nunc, vulputate et varius id, imperdiet nec nisl. </p>
-          <p className='text-base mb-[22px] text-[#262626] dark:text-[#e6e6e6]'></p>
+          <div className='grid grid-cols-1 gap-[33px] xl:grid-cols-4 sm:grid-cols-2'>
+            <div>
+              <h1 className='text'>Jannik Grothusen</h1>
+              <p className='text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras augue sapien, rutrum vitae lectus ut, aliquet tincidunt sem. Curabitur rutrum mi viverra, pulvinar ipsum id, aliquam dui. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
+              <p className='text'>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed cursus id purus in pretium. Duis porttitor eros pharetra sagittis mollis. Vivamus ut aliquet eros, ultricies interdum lectus. In mi nunc, vulputate et varius id, imperdiet nec nisl. </p>
+              <p className='text'></p>
+            </div>
+            <div>
+              <h1 className='text'>Jannik Grothusen</h1>
+              <p className='text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras augue sapien, rutrum vitae lectus ut, aliquet tincidunt sem. Curabitur rutrum mi viverra, pulvinar ipsum id, aliquam dui. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
+              <p className='text'>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed cursus id purus in pretium. Duis porttitor eros pharetra sagittis mollis. Vivamus ut aliquet eros, ultricies interdum lectus. In mi nunc, vulputate et varius id, imperdiet nec nisl. </p>
+              <p className='text'></p>
+            </div>
+            <div>
+              <h1 className='text'>Jannik Grothusen</h1>
+              <p className='text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras augue sapien, rutrum vitae lectus ut, aliquet tincidunt sem. Curabitur rutrum mi viverra, pulvinar ipsum id, aliquam dui. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
+              <p className='text'>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed cursus id purus in pretium. Duis porttitor eros pharetra sagittis mollis. Vivamus ut aliquet eros, ultricies interdum lectus. In mi nunc, vulputate et varius id, imperdiet nec nisl. </p>
+              <p className='text'></p>
+            </div>
+            <div>
+              <h1 className='text'>Jannik Grothusen</h1>
+              <p className='text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras augue sapien, rutrum vitae lectus ut, aliquet tincidunt sem. Curabitur rutrum mi viverra, pulvinar ipsum id, aliquam dui. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
+              <p className='text'>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed cursus id purus in pretium. Duis porttitor eros pharetra sagittis mollis. Vivamus ut aliquet eros, ultricies interdum lectus. In mi nunc, vulputate et varius id, imperdiet nec nisl. </p>
+              <p className='text'></p>
+            </div>
+          </div>
         </div>
       </main>
     </>
