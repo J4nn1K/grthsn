6fa676c4a7b57ff2b173b0502939cc547d7652dd
J4nn1K <jannikjorge@grothusen.de>
2023-01-16 23:56:51 -0800

    Make background full screen height

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <div className='p-[33px] bg-white dark:bg-black'>
+        <div className='p-[33px] h-screen bg-white dark:bg-black'>
           <h1 className='text-base mb-[22px] text-[#262626] dark:text-[#e6e6e6]'>Jannik Grothusen</h1>
           <p className='text-base mb-[22px] text-[#262626] dark:text-[#e6e6e6]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras augue sapien, rutrum vitae lectus ut, aliquet tincidunt sem. Curabitur rutrum mi viverra, pulvinar ipsum id, aliquam dui. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
           <p className='text-base mb-[22px] text-[#262626] dark:text-[#e6e6e6]'>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed cursus id purus in pretium. Duis porttitor eros pharetra sagittis mollis. Vivamus ut aliquet eros, ultricies interdum lectus. In mi nunc, vulputate et varius id, imperdiet nec nisl. </p>
