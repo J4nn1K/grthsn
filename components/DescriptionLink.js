16e6281a367c5662bd2c14357fed037b59a8f2c6
J4nn1K <jannikjorge@grothusen.de>
2023-01-17 23:40:44 -0800

    Create custom components for links

---
+++ components/DescriptionLink.js
@@ -0,0 +1,12 @@
+export default function DescriptionLink(props) {
+  return (
+      <p className='text flex flex-col'>
+        <a href={props.href} target="_blank" className="flex justify-left items-center group ">
+          <p className='group-hover:text-[#9e9e9e] transition'>{props.name}</p>
+          <p className='group-hover:rotate-[-45deg] text-[#9e9e9e] group-hover:text-[#262626] dark:group-hover:text-[#e6e6e6] ml-1 transition'> → </p>
+        </a> 
+        <br/>
+        {props.desc}
+      </p>
+  )
+}
