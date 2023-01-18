e5c950eb29009dc9dab7350fc27b53dc110d1928
J4nn1K <jannikjorge@grothusen.de>
2023-01-18 00:16:47 -0800

    Fix anchor highlighting

--- components/DescriptionLink.js
+++ components/DescriptionLink.js
@@ -1,12 +1,13 @@
 export default function DescriptionLink(props) {
   return (
-      <p className='text flex flex-col'>
-        <a href={props.href} target="_blank" rel="noreferrer" className="flex justify-left items-center group ">
+    <div>
+      <p className='text mb-0 flex block-inline'>
+        <a href={props.href} target="_blank" rel="noreferrer" className="flex justify-left items-center group">
           <p className='group-hover:text-[#9e9e9e] transition'>{props.name}</p>
           <p className='group-hover:rotate-[-45deg] text-[#9e9e9e] group-hover:text-[#262626] dark:group-hover:text-[#e6e6e6] ml-1 transition'> → </p>
         </a> 
-        <br/>
-        {props.desc}
       </p>
+      <p class="text">{props.desc}</p>
+    </div>
   )
 }
